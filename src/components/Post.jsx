90a455d176aeeaa7b16266295d9f570a486fdeed
WisleyASousa <wisleya.sousa@outlook.com.br>
2023-04-10 10:59:01 -0300

    finalizando sidebar, header, iniciando componente post

---
+++ src/components/Post.jsx
@@ -0,0 +1,30 @@
+import styles from './Post.module.css'
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src='https://github.com/wisleyasousa.png' />
+          <div className={styles.authorInfo}>
+            <strong>Wisley A. Sousa</strong>
+            <span>Dev Front-End</span>
+          </div>
+        </div>
+
+        <time 
+          title='10 de abril as 10:50'
+          dateTime='2023-04-10'>
+            Publicado há 1h
+        </time>
+      </header>
+
+      <div  className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+        <p>👉 <a href='#'>jane.design/doctorcare</a></p>
+        <p><a href='#'>#novoprojeto #nlw #rocketseat</a></p>
+      </div>
+    </article>
+  )
+}
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,26 @@
+import { PencilSimpleLine } from '@phosphor-icons/react'
+import styles from './Sidebar.module.css'
+
+export function Sidebar() {
+  return (
+      <aside className={styles.sidebar}>
+        <img 
+          className={styles.cover} 
+          src='https://images.unsplash.com/photo-1680950376343-4a14ef84157d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=300&q=50' />
+
+        <div className={styles.profile}>
+          <img className={styles.avatar} src='https://github.com/wisleyasousa.png' />
+
+          <strong>Wisley A. Sousa</strong>
+          <span>Web Developer</span>
+        </div>
+
+        <footer>
+          <a href='#'>
+            <PencilSimpleLine size={20} />
+            Editar seu Perfil
+          </a>
+        </footer>
+      </aside>
+    );
+}
