d190987e31a65e311a0356b009f42caaad6621e5
WisleyASousa <wisleya.sousa@outlook.com.br>
2023-04-11 09:39:43 -0300

    finalizando area de comentario, iniciando area de comentarios

---
+++ src/components/Comment.jsx
@@ -0,0 +1,39 @@
+import { ThumbsUp, Trash } from '@phosphor-icons/react'
+import styles from './Comment.module.css'
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img className={styles.avatar} src='https://github.com/wisleyasousa.png' />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Wisley A. Sousa</strong>
+              <time
+                title='10 de abril as 10:50'
+                dateTime='2023-04-10'>
+                Cerca de 1h atrás
+              </time>
+            </div>
+            <button title='Deletar comentário'>
+              <Trash size={20} />
+            </button>
+          </header>
+
+          <p>
+            Muito bom Devon, parabéns!! 👏👏
+          </p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp size={20} />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Comment } from './Comment'
 import styles from './Post.module.css'
 
 export function Post() {
@@ -28,6 +29,21 @@ export function Post() {
           <a href='#'>#nlw</a> 
           <a href='#'>#rocketseat</a>
         </p>
+      </div>
+
+      <form className={styles.comentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea
+          placeholder='Deixe um comentário'
+        />
+        <footer>
+          <button type='submit'>Publicar</button>
+        </footer>
+      </form>
+
+      <div className={styles.commentList}>
+        <Comment />
       </div>
     </article>
   )
